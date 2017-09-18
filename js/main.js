5a35166b0381abf031cf6c022da5b3145e61c1e1
Piotr Lewinski <piotrlew5@wp.pl>
2017-09-18 21:18:33 +0200

    bootstrap

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 function myMap() {
     var mapCanvas = document.getElementById("map");
     var mapOptions = {
-        center: new google.maps.LatLng(51.5, -0.2),
+        center: new google.maps.LatLng(52.2, 21.02),
         zoom: 10
     };
     var map = new google.maps.Map(mapCanvas, mapOptions);
